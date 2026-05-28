b5c6f94d3951e9bb5c1f0caa49f5273fe38da7ca
edcbiel <bilnabilla10@gmail.com>
2026-05-28 18:18:15 +0700

    Add Risk Profile and Medical Records UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,8 @@ import AIChatPage from './pages/AIChatPage'
 import HabitLogPage from './pages/HabitLogPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import RiskProfilePage from './pages/RiskProfilePage'
+import MedicalRecordsPage from './pages/MedicalRecordsPage'
 
 
 function App() {
@@ -18,8 +20,8 @@ function App() {
         <Route index element={<Navigate to="/chat" replace />} />
         <Route path="chat" element={<AIChatPage />} />
         <Route path="habit-log" element={<HabitLogPage />} />
-        {/* <Route path="risk-profile" element={<RiskProfilePage />} /> */}
-        {/* <Route path="medical-records" element={<MedicalRecordsPage />} /> */}
+        <Route path="risk-profile" element={<RiskProfilePage />} />
+        <Route path="/medical-records" element={<MedicalRecordsPage />} />
       </Route>
     </Routes>
   )
